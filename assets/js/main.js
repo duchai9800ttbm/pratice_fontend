5eb20087c4d44f2aedafc14512aa263d53fe6d74
smay <duchai9800ttbm@gmail.com>
2019-03-26 17:29:31 +0700

    20190326

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
     var owl = $('.owl-carousel');
 
-    owl.owlCarousel();
+    owl.owlCarousel({
+        rtl:false,
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: false,
+        nav: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
     // Go to the next item
     $('.owl-next').click(function() {
         owl.trigger('next.owl.carousel');
