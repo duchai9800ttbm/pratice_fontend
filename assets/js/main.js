f3629c068037f19791be1efd3cababffc64af801
smay <duchai9800ttbm@gmail.com>
2019-03-29 16:02:14 +0700

    20190329

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-    var owl = $('.owl-carousel');
-
-    owl.owlCarousel({
+    $('.owl-product1').owlCarousel({
         rtl:false,
         loop:true,
         margin:10,
@@ -20,14 +18,59 @@ $(document).ready(function(){
             }
         }
     });
-    // Go to the next item
-    $('.owl-next').click(function() {
-        owl.trigger('next.owl.carousel');
+
+    $('.owl-product2').owlCarousel({
+        rtl:false,
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: false,
+        nav: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    $('.owl-next-banner').click(function() {
+        $('.owl-banner').trigger('next.owl.carousel');
     })
-    // Go to the previous item
-    $('.owl-prev').click(function() {
-        // With optional speed parameter
-        // Parameters has to be in square bracket '[]'
-        owl.trigger('prev.owl.carousel', [300]);
+
+    $('.owl-prev-banner').click(function() {
+        $('.owl-banner').trigger('prev.owl.carousel', [300]);
     })
+
+    $('.owl-next-product1').click(function() {
+        $('.owl-product1').trigger('next.owl.carousel');
+    })
+
+    $('.owl-prev-product1').click(function() {
+        $('.owl-product1').trigger('prev.owl.carousel', [300]);
+    })
+
+    $('.owl-next-product2').click(function() {
+        $('.owl-product2').trigger('next.owl.carousel');
+    })
+
+    $('.owl-prev-product2').click(function() {
+        $('.owl-product2').trigger('prev.owl.carousel', [300]);
+    })
+
+    $('.owl-banner').owlCarousel({
+        items:1,
+        loop: false,
+        center:true,
+        dots: false,
+        margin:10,
+        URLhashListener:true,
+        autoplayHoverPause:true,
+        startPosition: 'URLHash'
+    });
 });
