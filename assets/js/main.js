f48dfab6153a49c37f340d51b52f3c63ccf493b7
smay <duchai9800ttbm@gmail.com>
2019-03-30 12:09:37 +0700

    20190330

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,4 +73,19 @@ $(document).ready(function(){
         autoplayHoverPause:true,
         startPosition: 'URLHash'
     });
+
+    var btn = $("#back-to-top");
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 50) {
+          btn.addClass('show');
+        } else {
+          btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+    });
 });
